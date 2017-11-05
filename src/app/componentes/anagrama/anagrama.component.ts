49a278c6cc060c04300ac7117900db091b6d55e5
tama <tamaravcristaldo@gmail.com>
2017-11-05 10:11:14 -0300

    logica de anagrama

--- src/app/componentes/anagrama/anagrama.component.ts
+++ src/app/componentes/anagrama/anagrama.component.ts
@@ -8,11 +8,56 @@ import { JuegoAnagrama } from '../../clases/juego-anagrama';
 })
 export class AnagramaComponent implements OnInit {
 
-
+  nuevoJuego: JuegoAnagrama;
+  Mensajes:string;
+  contador:number;
+  ocultarVerificar:boolean;
+  mensaje : string;
 
   constructor() {
-
+    this.nuevoJuego = new JuegoAnagrama(); 
+    this.ocultarVerificar=true;
    }
+
+   generarPalabra() {
+    this.nuevoJuego.asignarPalabra();
+    this.contador=0;
+    this.ocultarVerificar = false;
+    this.nuevoJuego.gano = false;
+    this.Mensajes = "";
+  }
+
+  verificar()
+  {
+    this.contador++;
+    this.ocultarVerificar=true;
+    if (this.nuevoJuego.verificar()){
+      this.MostarMensaje("Sos un Genio!!!",true);
+    }else{
+      this.mensaje = "Ooops, casi lo lograste!";
+      this.MostarMensaje(this.mensaje); 
+    }
+    console.info("Gano: ",this.nuevoJuego.gano);  
+  }  
+
+  MostarMensaje(mensaje:string,gano:boolean=false) {
+    this.Mensajes = mensaje;    
+    var x = document.getElementById("msj");
+    if(gano)
+      {
+        x.className = "show Ganador";
+      }else{
+        x.className = "show Perdedor";
+      }
+
+    var modelo = this;
+    setTimeout(function(){ 
+      x.className = x.className.replace("show", "");
+      modelo.ocultarVerificar=false;
+     }, 3000);
+    console.info("objeto",x);
+  
+   }  
 
 
 
